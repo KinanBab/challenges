b8998d36f9d00c19c14b526c4638727185a9f96e
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2018-11-08 03:03:06 -0500

    Median of two sorted arrays in Java and C both faster than 100% solutions

---
+++ leetcode-median-of-two-sorted-arrays/Solution.c
@@ -0,0 +1,116 @@
+#include <limits.h>
+
+static int n;
+static int m;
+static int l;
+static int i;
+
+int myBinarySearch(int* nums1, int* nums2, int start, int end) { 
+    while(!(start == end || start >= n)) {
+        int mid = (start + end) / 2; // bug if start+end overflow!
+        int dir = checkIfMedian(nums1, nums2, mid);
+        if(dir == 0) {
+            return mid;
+        } else if(dir < 0) {
+            end = mid;
+        } else {
+            start = mid+1;
+        }
+    }
+    return -1;
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    if(nums1Size < nums2Size) {
+        nums1Size += nums2Size;
+        nums2Size = nums1Size - nums2Size;
+        nums1Size = nums1Size - nums2Size;
+        int* tmp = nums1;
+        nums1 = nums2;
+        nums2 = tmp;
+    }
+    
+    n = nums1Size;
+    m = nums2Size;
+    l = n + m;
+    i = (l - 1) / 2;
+
+    if(m == 0) {
+        double median = nums1[i];
+        if(n % 2 == 0) {
+            median = (median + nums1[i+1]) / 2.0;
+        }
+        return median;
+    }
+    
+    // main code
+    int left = 1;
+    int k = myBinarySearch(nums1, nums2, 0, n);
+    if (k == -1) {
+        n = nums2Size;
+        m = nums1Size;
+        k = myBinarySearch(nums2, nums1, 0, n);
+        left = 0;
+        n = nums1Size;
+        m = nums2Size;
+    }
+
+    int median = left == 1 ? nums1[k] : nums2[k];
+    if(l % 2 == 1) {
+        return median;
+    }
+
+    // must get next largest element, sum, and divide by 2
+    int check = i - k - 1;
+    int candidate1 = INT_MAX;
+    int candidate2 = INT_MAX;
+    if(left) {
+        if (k+1 < n) candidate1 = nums1[k+1];
+        if (check+1 < m) candidate2 = nums2[check+1];
+    } else {
+        if (k+1 < m) candidate1 = nums2[k+1];
+        if (check+1 < n) candidate2 = nums1[check+1];
+    }
+
+    median = median + (candidate1 < candidate2 ? candidate1 : candidate2);
+    return median / 2.0;
+}
+
+int checkIfMedian(int* nums1, int* nums2, int k) {
+    int candidate = nums1[k];
+
+    // we know there are exactly k elements less than nums1[k] in nums1 alone,
+    // therefore nums1[k] is the element we are looking for if
+    // there are exactly i - k in nums2.
+    int check = i - k;
+    if(check < 0) {
+        return -1;
+    }
+    if(check > m) {
+        return 1;
+    }
+
+    if(check == 0) {
+        if(candidate <= nums2[0]) {
+            return 0;
+        }
+        return -1;
+    }
+
+    if(check == m) {
+        if(nums2[m-1] <= candidate) {
+            return 0;
+        }
+        return 1;
+    }
+
+    // check > 0, check < nums2.length
+    if(candidate > nums2[check]) {
+        return -1;
+    }
+    if(nums2[check-1] > candidate) {
+        return 1;
+    }
+
+    return 0;
+}
---
+++ leetcode-median-of-two-sorted-arrays/Solution.java
@@ -0,0 +1,106 @@
+class Solution {
+    public int myBinarySearch(int[] nums1, int[] nums2, int start, int end) { 
+        while(!(start == end || start >= nums1.length)) {
+            int mid = (start + end) / 2; // bug if start+end overflow!
+            int dir = checkIfMedian(nums1, nums2, mid);
+            if(dir == 0) {
+                return mid;
+            } else if(dir < 0) {
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length < nums2.length) {
+            int[] tmp = nums2;
+            nums2 = nums1;
+            nums1 = tmp;
+        }
+
+        if(nums2.length == 0) {
+            int k = (nums1.length - 1)/2;
+            double median = nums1[k];
+            if(nums1.length % 2 == 0) {
+                median += nums1[k+1];
+                return median / 2;
+            }
+            return median;
+        }
+
+        boolean left = true;
+        int k = myBinarySearch(nums1, nums2, 0, nums1.length);
+        if (k == -1) {
+            k = myBinarySearch(nums2, nums1, 0, nums2.length);
+            left = false;
+        }
+
+        int median = left ? nums1[k] : nums2[k];
+        if((nums1.length + nums2.length) % 2 == 1) {
+            return median;
+        }
+        
+        // must get next largest element, sum, and divide by 2
+        int check = (nums1.length + nums2.length - 1) / 2 - k - 1;
+        int candidate1 = Integer.MAX_VALUE;
+        int candidate2 = Integer.MAX_VALUE;
+        if(left) {
+            if (k+1 < nums1.length) candidate1 = nums1[k+1];
+            if (check+1 < nums2.length) candidate2 = nums2[check+1];
+        } else {
+            if (k+1 < nums2.length) candidate1 = nums2[k+1];
+            if (check+1 < nums1.length) candidate2 = nums1[check+1];
+        }
+
+        median = median + Math.min(candidate1, candidate2);
+        return median / 2.0;
+    }
+    
+    public int checkIfMedian(int[] nums1, int[] nums2, int k) {
+        int candidate = nums1[k];
+
+        // indices
+        int n = nums1.length;
+        int m = nums2.length;
+        int l = n + m;
+        int i = (l - 1) / 2; // alright even if floored
+
+        // we know there are exactly k elements less than nums1[k] in nums1 alone,
+        // therefore nums1[k] is the element we are looking for if
+        // there are exactly i - k in nums2.
+        int check = i - k;
+        if(check < 0) {
+            return -1;
+        }
+        if(check > nums2.length) {
+            return 1;
+        }
+
+        if(check == 0) {
+            if(candidate <= nums2[0]) {
+                return 0;
+            }
+            return -1;
+        }
+
+        if(check == nums2.length) {
+            if(nums2[nums2.length-1] <= candidate) {
+                return 0;
+            }
+            return 1;
+        }
+        
+        // check > 0, check < nums2.length
+        if(candidate > nums2[check]) {
+            return -1;
+        }
+        if(nums2[check-1] > candidate) {
+            return 1;
+        }
+        
+        return 0;
+    }
+}
